756a83cd49ba4d3c0b4b59644bef3bfba993e92a
llckumadevelop <kumauser01@kuma.ink>
2024-07-01 08:31:24 +0700

    update style component

---
+++ src/StyledComponents/DemoJSS.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { Button } from "../Components/Button";
+class DemoJSS extends Component {
+  render() {
+    return (
+      <div>
+        <Button>Click me!</Button>
+      </div>
+    );
+  }
+}
+
+export default DemoJSS;
